91abc68b0e18ca418bf9f5e36c55b688631279cc
Ruta Reisoglu <rutareiso@protonmail.com>
2022-03-18 00:50:24 -0400

    Add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "../styles/Footer.module.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={`py-2 ${styles.footer}`}>
+      <p className={`text-center ${styles.brand}`}>Oyun Treni</p>
+      <div className="d-flex justify-content-center">
+        <p className="px-2">
+          <span>Telefon: </span>+ 345 295 3577
+        </p>
+        <p className="px-2">Istanbul, Turkiye</p>
+      </div>
+
+      <div
+        className={`d-flex px-2 pt-2 justify-content-center ${styles.creator}`}
+      >
+        <p>Created by RReiso</p>
+        <a
+          className="link px-2"
+          rel="noreferrer"
+          href="https://github.com/rreiso"
+          target="_blank"
+        >
+          <p className="sr-only">RReiso github.com profile</p>
+          <i
+            className={`fa fa-github fa-lg ${styles.github}`}
+            aria-hidden="true"
+          ></i>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
