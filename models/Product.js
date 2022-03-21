432f9e3355982f08f13860b2e510efa8048f11cf
Ruta Reisoglu <rutareiso@protonmail.com>
2022-03-20 23:07:48 -0400

    Create schema for Product

--- models/Product.js
+++ models/Product.js
@@ -1,3 +1,27 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema();
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 120,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxlength: 700,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
